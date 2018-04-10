bdc000c4e43e2a1afd5781d393d803fb6a14930f
webharry <1612677524@qq.com>
2018-04-10 11:56:51 +0800

    up

---
+++ README.md
@@ -0,0 +1,35 @@
+# vue-UI
+
+`vue-UI` 是一款基于 `Vue.js 2.0` 的前端 UI 组件库，主要用于快速开发 PC 网站中后台产品
+[演示地址](https://github.com/webharry/vue-UI)
+
+
+## 特性
+
+- 基于 `Vue` 开发的 UI 组件
+- 使用 npm + webpack + babel 的工作流，支持 ES2015
+- 提供友好的 API，可灵活的使用组件
+
+
+## 安装
+
+- yarn
+
+```bash
+yarn add vueui
+```
+
+- npm 
+
+```bash
+npm install vueui --save
+```
+
+## 使用
+
+```js
+import vueUI from 'vueui' // 引入组件库
+import 'vueui/packages/theme-default/lib/index.css' // 引入样式库
+
+Vue.use(vueUI)
+```
---
+++ packages/button/src/button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button>
+    <i v-if="icon !== ''" :class="icon"></i>
+    <slot></slot>
+  </button>
+</template>
+<script>
+  export default{
+    name: 'VButton',
+    props: {
+      type: {
+        type: String,
+        default: 'default'
+      },
+      size: {
+        type: String,
+        default: 'default'
+      },
+      icon: {
+        type: String,
+        default: ''
+      },
+      plain: Boolean,
+      disabled: Boolean,
+      round: Boolean
+    },
+    data () {
+      return {
+        msg: 'button'
+      }
+    }
+  }
+</script>
